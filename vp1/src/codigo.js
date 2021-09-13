558c72ff629aed338f098fa492df0a0c34820f71
Davi Ferreira <daviferreirart@gmail.com>
2021-09-13 13:21:36 -0300

    primeira parte do trabalho

---
+++ vp1/src/codigo.js
@@ -0,0 +1,39 @@
+let contadorLike = 0;
+let contadorComentario = 0
+function deuLike() {
+
+    const botaoCurtir = document.getElementById("botaoCurtir");
+    if (botaoCurtir.innerHTML === 'Curtir') {
+        contadorLike++;
+        botaoCurtir.innerHTML = 'Remover'
+        botaoCurtir.classList.remove('btn-primary')
+        botaoCurtir.classList.add('btn-danger');
+    } else {
+        contadorLike--;
+        botaoCurtir.innerHTML = 'Curtir'
+        botaoCurtir.classList.remove('btn-danger')
+        botaoCurtir.classList.add('btn-primary');
+
+
+    }
+    const qntLikes = document.getElementById("likes");
+    qntLikes.innerHTML = `${contadorLike} curtidas`
+
+
+}
+
+function adicionarComentario() {
+    const comentario = document.getElementById("comentario").value
+    if(comentario === ""){
+        return
+    }
+    const divComentario = document.getElementById("comments")
+
+    const p = document.createElement("p")
+    p.appendChild(document.createTextNode(`${comentario}`))
+    divComentario.appendChild(p)
+    
+    contadorComentario++;
+    const divQntComentario = document.getElementById("qntComentarios")
+    divQntComentario.innerHTML = `${contadorComentario} comentários`
+}
