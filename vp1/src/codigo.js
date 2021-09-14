c6ade7ac4aefdcec7f3c66f223cd1c56d86a4dcb
Davi Ferreira <daviferreirart@gmail.com>
2021-09-14 14:17:51 -0300

    versão final instafeed

--- vp1/src/codigo.js
+++ vp1/src/codigo.js
@@ -1,39 +1,38 @@
-let contadorLike = 0;
-let contadorComentario = 0
-function deuLike() {
+let contadorLike = [0, 0, 0];
+let contadorComentario = [0, 0, 0]
+function deuLike(num) {
 
-    const botaoCurtir = document.getElementById("botaoCurtir");
+    const botaoCurtir = document.getElementById(`botaoCurtir-${num}`);
     if (botaoCurtir.innerHTML === 'Curtir') {
-        contadorLike++;
+        contadorLike[num]++;
+    
         botaoCurtir.innerHTML = 'Remover'
         botaoCurtir.classList.remove('btn-primary')
         botaoCurtir.classList.add('btn-danger');
     } else {
-        contadorLike--;
+        contadorLike[num]--;
         botaoCurtir.innerHTML = 'Curtir'
         botaoCurtir.classList.remove('btn-danger')
         botaoCurtir.classList.add('btn-primary');
 
 
     }
-    const qntLikes = document.getElementById("likes");
-    qntLikes.innerHTML = `${contadorLike} curtidas`
-
-
+    const qntLikes = document.getElementById(`likes-${num}`);
+    qntLikes.innerHTML = `${contadorLike[num]} curtidas`
 }
 
-function adicionarComentario() {
-    const comentario = document.getElementById("comentario").value
-    if(comentario === ""){
+function adicionarComentario(num) {
+    const comentario = document.getElementById(`comentario-${num}`).value
+    if (comentario === "") {
         return
     }
-    const divComentario = document.getElementById("comments")
+    const divComentario = document.getElementById(`comments-${num}`)
 
     const p = document.createElement("p")
     p.appendChild(document.createTextNode(`- ${comentario}`))
     divComentario.appendChild(p)
-    
-    contadorComentario++;
-    const divQntComentario = document.getElementById("qntComentarios")
-    divQntComentario.innerHTML = `${contadorComentario} comentários`
+
+    contadorComentario[num]--;
+    const divQntComentario = document.getElementById(`qntComentarios-${num}`)
+    divQntComentario.innerHTML = `${contadorComentario[num]} comentários`
 }
